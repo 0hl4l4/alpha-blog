f5c44d7dba2680ae5a3e6aa298dccf13609a53d6
Jayson Puerto <jpuert032@gmail.com>
2022-07-13 12:59:13 +0800

    Added Update controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,5 +20,20 @@ class ArticlesController < ApplicationController
         render 'new'
       end
     end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = "Article was updated successfully."
+        redirect_to article_path(@article)
+      else
+        render 'edit'
+      end
+    end
+
 end 
   
